5bf81be544042c69d8f39b2637fef17ba08ff7e0
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-07-17 21:31:27 +0530

    Blog_Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
 import './App.css';
+import Article from './compo/Article';
+import Food from './compo/Food';
+import Fitness from './compo/Fitness';
+import Hollywood from './compo/Hollywood';
+import Bollywood from './compo/Bollywood';
+import Technology from './compo/Technology';
+import Home from './compo/Home';
+import {ProvideData} from "./compo/ContextApi.js";
+import './compo/index.css'
+import Header from "./compo/Header.js"
+import Footer from './compo/Footer';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ProvideData>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/bollywood" element={<Bollywood />} />
+        <Route path="/hollywood" element={<Hollywood />} />
+        <Route path="/fitness" element={<Fitness />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/article/:path" element={<Article />} />
+      </Routes>
+      <Footer />
+      </ProvideData>
+    </Router>    
   );
 }
 
